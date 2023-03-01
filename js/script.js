cbb57eed0b151d99776f491a5ba088f23d7b51e5
GiuliaSetti <giulia.setti96@gmail.com>
2023-03-01 14:48:08 +0100

    scaffolding + pseudocode + titoli

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Consegna:
+// Data una lista della spesa (memorizzata in un array), stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+
+
+/* - creo un'array
+   - creo una variabile contatore che possa interrompere il ciclo di while.
+
+   WHILE (FINO A CHE) il contatore non raggiunge la lunghezza dell'array -
+
+        ° collego la ul (lista della spesa) in js
+        ° crea un elemento (li)
+        ° lo collego alla lista della spesa .append()
+        ° stampo l'elemento in pagina
+
+*/
